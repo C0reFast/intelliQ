bcae9723df0a126355baa81a1263971456f323ec
C0reFast <topchenfu@qq.com>
2014-02-13 19:45:56 +0800

    add monkey path of requests

--- common.py
+++ common.py
@@ -3,6 +3,26 @@
 
 """ Common Definitions
 """
+import requests
+
+
+def monkey_patch_requests():
+    """requests库中文乱码补丁
+    """
+    prop = requests.models.Response.content
+
+    def content(self):
+        _content = prop.fget(self)
+        if self.encoding == 'ISO-8859-1':
+            encodings = requests.utils.get_encodings_from_content(_content)
+            if encodings:
+                self.encoding = encodings[0]
+            else:
+                self.encoding = self.apparent_encoding
+            _content = _content.decode(self.encoding, 'replace').encode('utf8', 'replace')
+            self._content = _content
+        return _content
+    requests.models.Response.content = property(content)
 
 
 class Paper(object):
