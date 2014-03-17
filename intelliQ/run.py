a4020e7c89da128e757b3053550a03e3406cf00d
C0reFast <topchenfu@qq.com>
2014-03-17 16:23:09 +0800

    change the search exp and remove unuseful prints

--- intelliQ/run.py
+++ intelliQ/run.py
@@ -8,16 +8,15 @@ import parser
 
 
 if __name__ == '__main__':
-    spider_vip = Spider('vip_papers')
+    spider_paper = Spider('paper')
     spider_news = Spider('news')
-    search_exp = u'(Keyword_C=明矾+Title_C=明矾)'
+    search_exp = u'(Keyword_C=数据挖掘+Title_C=数据挖掘)'
     reqs = parser.page_parser(search_exp)
-    print reqs
     for req in reqs:
-        spider_vip.add_request(req)
-    spider_news.add_request(Request(url='http://tech.sina.com.cn/',
+        spider_paper.add_request(req)
+    spider_news.add_request(Request(url='http://roll.tech.sina.com.cn/s/channel.php#col=96',
                                     parser=parser.news_parser))
     spider_news.add_request(Request(url='http://jtt.zj.gov.cn/col/col16/index.html',
                                     parser=parser.news_parser))
     spider_news.crawl()
-    spider_vip.crawl()
+    spider_paper.crawl()
