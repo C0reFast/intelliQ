a9bb4422f3f2fbe42741d51ee00924ce316be276
C0reFast <topchenfu@qq.com>
2014-04-21 18:43:18 +0800

    use html2text to extract the main text

--- intelliQ/text_extractor.py
+++ intelliQ/text_extractor.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from pyquery import PyQuery as pq
-from lxml.html.clean import clean_html
-import re
-
-
-# 需要删除的一些内联标签
-DEL_TAG_RE = [re.compile('<{name}.*?>|</{name}>'.format(name=name), re.IGNORECASE)
-              for name in ('span', 'b', 'u', 'i', 'a', 'font', 'strong')]
+import html2text
 
 
 def extract(page_content):
-    """根据网页源代码解析网页主体内容"""
-    page_content = clean_html(page_content)
-    for r in DEL_TAG_RE:
-        page_content = r.sub('', page_content)
-    p = pq(page_content)
-    p('script').remove()
-    p('style').remove()
-    pags = p.text().split(' ')
-    return '\n'.join([x for x in pags if len(x) > 76])
+    h = html2text.HTML2Text(bodywidth=0)
+    h.ignore_links = True
+    h.ignore_images = True
+    split_text = h.handle(page_content).split('\n')
+    return '\n'.join([x for x in split_text if len(x) > 65])
